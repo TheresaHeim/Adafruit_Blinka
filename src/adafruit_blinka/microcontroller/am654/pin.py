bf1b7e03ec17f78184d1f955afcc0af7637f6146
TheresaHeim <theresa@familie-heimerl.de>
2020-11-24 15:11:06 +0000

    Änderung

--- src/adafruit_blinka/microcontroller/am654/pin.py
+++ src/adafruit_blinka/microcontroller/am654/pin.py
@@ -5,8 +5,8 @@
 import mraa
 import time
 
-Gpio.setmode(Gpio.BCM)  #Use BCM pins D4 = GPIO #4
-Gpio.setwarnings(False) #shh!
+#mraa.Gpio.setmode(mraa.Gpio.BCM)  #Use BCM pins D4 = GPIO #4
+#mraa.Gpio.setwarnings(False) #shh!
 
 
 class Pin: 
@@ -39,22 +39,22 @@ class Pin:
         if mode is not None:
             if mode == self.write:
                 self._mode = self.write()
-                Gpio.setup(self.id, Gpio.write())
+                mraa.Gpio.setup(self.id, mraa.Gpio.write())
             if mode == self.read:
                 self._mode = slef.read()
-                Gpio.setup(self.id, Gpio.read())
+                mraa.Gpio.setup(self.id, mraa.Gpio.read())
             elif mode == self.OUT:
                 self._mode = self.OUT
-                Gpio.setup(self.id, Gpio.OUT)
+                mraa.Gpio.setup(self.id, mraa.Gpio.OUT)
             else: 
                 raise RuntimeError("Invalid mode for pin: %s" % self.id)
         if pull is not None:
             if self._mode != self.IN:
                 raise RuntimeError("Cannot set pull resistor on output")
             if pull == self.PULL_UP:
-                Gpio.setup(self.id, Gpio.IN, pull_up_down=Gpio.PUD_UP)
+                mraa.Gpio.setup(self.id, mraa.Gpio.IN, pull_up_down=mraa.Gpio.PUD_UP)
             elif pull == self.PULL_DOWN:
-                Gpio.setup(self.id, Gpio.IN, pull_up_down=Gpio.PUD_DOWN)
+                mraa.Gpio.setup(self.id, mraa.Gpio.IN, pull_up_down=mraa.Gpio.PUD_DOWN)
             else:
                 raise RuntimeError("Invalid pull for pin: %s" % self.id)
 
@@ -63,14 +63,14 @@ class Pin:
             if val is not None:
                 if val == self.LOW:
                     self._value = val
-                    Gpio.output(self.id, val)
+                    mraa.Gpio.output(self.id, val)
                 elif val == self.HIGH:
                     self._value = val
-                    Gpio.output(self.id, val)
+                    mraa.Gpio.output(self.id, val)
                 else: 
                     raise RuntimeError("Invalid value for pin")
                 return None
-            return GPIO.input(self.id)
+            return mraa.Gpio.input(self.id)
 
 
 #Digital Pins
